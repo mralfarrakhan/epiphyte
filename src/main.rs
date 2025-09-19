6148f8c4603c2f5c142687b3985ba772f91ba24e
dead man <alfarrakhan@gmail.com>
2025-09-20 00:19:53 +0700

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,124 @@
+use std::{error::Error, net::SocketAddr, sync::mpsc, thread, time::Duration};
+
+use axum::{Router, response::Json, routing::get, serve};
+use dll_syringe::{
+    Syringe,
+    process::{OwnedProcess, Process},
+};
+use serde_json::json;
+use tokio::{net::TcpListener, runtime::Builder, signal};
+
+const DEFAULT_PORT: &str = "8070";
+const USAGE_MSG: &str = "usage: injector PROC_NAME PAYLOAD_PATH [PORT]";
+
+enum Command {
+    OffsetCmd,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut args = std::env::args();
+
+    let _ = args.next();
+    let target_name = args.next().ok_or(USAGE_MSG)?;
+    let payload_path = args.next().ok_or(USAGE_MSG)?;
+    let port = args.next().unwrap_or(DEFAULT_PORT.into());
+
+    if let Some(target_process) = OwnedProcess::find_first_by_name(&target_name) {
+        let pid = target_process.pid()?;
+        let base_name = target_process
+            .base_name()?
+            .to_str()
+            .unwrap_or("UNKNOWN BASE NAME")
+            .to_string();
+        let exec_path = target_process
+            .path()?
+            .to_str()
+            .unwrap_or("UNKNOWN EXEC PATH")
+            .to_string();
+
+        println!(
+            "injected process base name: {}, path: {}, pid: {}.",
+            base_name, exec_path, pid
+        );
+
+        let syringe = Syringe::for_process(target_process);
+        let injected_payload = syringe.inject(payload_path)?;
+
+        let (cmd_tx, cmd_rx) = mpsc::channel();
+
+        let info = async move || {
+            Json(json!({
+                "base name": base_name,
+                "exec path": exec_path,
+                "pid": pid,
+            }))
+        };
+
+        let offset_tx = cmd_tx.clone();
+        let thandle = thread::spawn(move || {
+            let runtime = Builder::new_current_thread().enable_all().build().unwrap();
+
+            runtime.block_on(async {
+                let app = Router::new().route("/", get(info)).route(
+                    "/offset",
+                    get(async move || offset_tx.send(Command::OffsetCmd).unwrap()),
+                );
+
+                let addr: SocketAddr = format!("127.0.0.1:{}", port).parse().unwrap();
+                let listener = TcpListener::bind(addr).await.unwrap();
+
+                serve(listener, app)
+                    .with_graceful_shutdown(shutdown_signal())
+                    .await
+                    .unwrap();
+            });
+        });
+
+        let remote_offset = unsafe {
+            syringe.get_raw_procedure::<extern "system" fn()>(injected_payload, "offset")
+        }?
+        .ok_or("error fetching remote procedure")?;
+
+        loop {
+            match cmd_rx.recv_timeout(Duration::from_millis(500)) {
+                Ok(Command::OffsetCmd) => remote_offset.call()?,
+                Err(mpsc::RecvTimeoutError::Timeout) => {
+                    if thandle.is_finished() {
+                        break;
+                    }
+                }
+                Err(mpsc::RecvTimeoutError::Disconnected) => break,
+            }
+        }
+
+        if let Err(e) = thandle.join() {
+            Err(format!("axum thread closed with panic: {:#?}", e))?;
+        } else {
+            println!("All good. Ejecting payload...");
+        }
+
+        syringe.eject(injected_payload)?;
+    } else {
+        eprintln!(
+            "Program whose name contains '{}' doesn't seem to be run...",
+            target_name
+        );
+    }
+
+    Ok(())
+}
+
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to set ctrl+c handler");
+    };
+
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
+}
